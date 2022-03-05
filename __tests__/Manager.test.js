99b90c667e6a13a13cda633cec696a77883ac924
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-05 11:57:13 -0500

    Sigh.......fixed simple errors in expect method sections

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,7 +2,7 @@ const Manager = require('../lib/Manager');
 const manager = new Manager('Mike','245036','M.Jammeson@hotmail.com','666');
 
 it('should be testing to see if we get values from constructor for the manager object', () => {
-expect(manager.name).toBe('Mike');
+    expect(manager.name).toBe('Mike');
     expect(manager.id).toBe('245036');
     expect(manager.email).toBe('M.Jammeson@hotmail.com');
     expect(manager.officeId).toBe('666');
