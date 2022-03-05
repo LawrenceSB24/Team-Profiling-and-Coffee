7291271d37c7552f647ae87e090b7e1ae154773e
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-05 14:43:29 -0500

    Fixing error with team cards

--- source/coffee.js
+++ source/coffee.js
@@ -85,16 +85,14 @@ module.exports = teamMates => {
         <div class = 'jumbotron display-4'> My Team </div>
 
         <main>
-            <div class = "card">
-                <div class = "card-body">
-                    ${coffeeGroup(teamMates)}
-                </div>
-            </div>
+            
+            ${coffeeGroup(teamMates)}
+            
         </main>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
     </html>
 
-    `
+    `;
 }
