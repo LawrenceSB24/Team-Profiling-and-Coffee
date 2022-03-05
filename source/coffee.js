eb20258189d6da1bc63f694fce2b7ca72b1c8f5f
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-05 18:50:20 -0500

    Added card formatting from Bootstrap and fixed typo issues

--- source/coffee.js
+++ source/coffee.js
@@ -10,12 +10,13 @@ const coffeeGroup = (teamMates) => {
                 <h2>${manager.name}</h2> 
                 <br/>
             </div>
-            <div class = "card-body"
+            <div class = "card-body">
                 <div class = "card-text">
-                    <ul>
-                        <li> Id: ${manager.id} </li>
-                        <li> Email Address:<a href = "${manager.email}"> ${manager.email}</a></li>
-                        <li> Office ID: ${manager.officeId} </li>
+                    <h3> Manager </h3>
+                    <ul class = "list-group list-group-flush">
+                        <li class = "list-group-item"><h4> Id: ${manager.id} </h4></li>
+                        <li class = "list-group-item"><h4> Email Address:<a href = "${manager.email}"> ${manager.email}</a></h4></li>
+                        <li class = "list-group-item"><h4> Office ID: ${manager.officeId} </h4></li>
                     </ul>
                 </div>
         </div>`;
@@ -30,12 +31,13 @@ const coffeeGroup = (teamMates) => {
                 <h2>${engineer.name}</h2> 
                 <br/>
             </div>
-            <div class = "card-body"
+            <div class = "card-body">
                 <div class = "card-text">
-                    <ul>
-                        <li> Id: ${engineer.id} </li>
-                        <li> Email Address:<a href = "${engineer.email}"> ${engineer.email}</a></li>
-                        <li> GitHub Username: ${engineer.gitName} </li>
+                    <h3> Engineer </h3>
+                    <ul class = "list-group list-group-flush">
+                        <li class = "list-group-item"><h4> Id: ${engineer.id} </h4></li>
+                        <li class = "list-group-item"><h4> Email Address:<a href = "${engineer.email}"> ${engineer.email}</a></h4></li>
+                        <li class = "list-group-item"><h4> GitHub Username: ${engineer.gitName} </h4></li>
                     </ul>
                 </div>
         </div>`;
@@ -50,12 +52,13 @@ const coffeeGroup = (teamMates) => {
                 <h2>${intern.name}</h2> 
                 <br/>
             </div>
-            <div class = "card-body"
+            <div class = "card-body">
                 <div class = "card-text">
-                    <ul>
-                        <li> Id: ${intern.id} </li>
-                        <li> Email Address:<a href = "${intern.email}"> ${intern.email}</a></li>
-                        <li> School: ${intern.school} </li>
+                    <h3> Intern </h3>
+                    <ul class = "list-group list-group-flush">
+                        <li class = "list-group-item"><h4> Id: ${intern.id} </h4></li>
+                        <li class = "list-group-item"><h4> Email Address:<a href = "${intern.email}"> ${intern.email}</a></h4></li>
+                        <li class = "list-group-item"><h4> School: ${intern.school} </h4></li>
                     </ul>
                 </div>
         </div>`;
@@ -79,32 +82,32 @@ const coffeeGroup = (teamMates) => {
     return teamWeb.join('');
 }
 
-module.exports = teamMates => {return `<!DOCTYPE html>
-<html lang="en">
+module.exports = teamWeb => {return `<!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../dist/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="../dist/style.css">
 
-    <title>Team Profiling and Coffee</title>
-</head>
+        <title>Team Profiling and Coffee</title>
+    </head>
 
-<body>
-    <div class='jumbotron display-4'> My Team </div>
+    <body>
+        <div class='jumbotron display-4'> My Team </div>
 
-    <main>
-        ${coffeeGroup(teamWeb)}
-    </main>
+        <main>
+            ${coffeeGroup(teamWeb)}
+        </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+    </body>
 
-</html>`;
+    </html>`;
 }
