321d57a9aa2c5061bf8e4ad5112783fafe0d57d2
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-05 12:23:37 -0500

    Added styling to generated html file

--- source/coffee.js
+++ source/coffee.js
@@ -77,7 +77,7 @@ module.exports = teamMates => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel = "stylesheet" href = "../dist/style.css"
+        <link rel = "stylesheet" href = "../dist/style.css">
 
         <title>Team Profiling and Coffee</title>
     </head>
@@ -85,7 +85,11 @@ module.exports = teamMates => {
         <div class = 'jumbotron display-4'> My Team </div>
 
         <main>
-            ${coffeeGroup(teamMates)}
+            <div class = "card">
+                <div class = "card-body">
+                    ${coffeeGroup(teamMates)}
+                </div>
+            </div>
         </main>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
