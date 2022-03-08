fc5e0acb3d7b7932cdb6dc76d9fc9d36be6c3879
Lawrence Blundo <lawrs2022@gmail.com>
2022-03-07 21:00:29 -0500

    Added comments for testing of Manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,12 @@
+// Test for the Manager class
+
+// Retrieval of Manager module
 const Manager = require('../lib/Manager');
+
+// Variable for setting consturctor elements of Manager class as placeholder values
 const manager = new Manager('Mike','245036','M.Jammeson@hotmail.com','666');
 
+// Test of placing values into the Manager onject
 it('should be testing to see if we get values from constructor for the manager object', () => {
     expect(manager.name).toBe('Mike');
     expect(manager.id).toBe('245036');
@@ -8,22 +14,29 @@ it('should be testing to see if we get values from constructor for the manager o
     expect(manager.officeId).toBe('666');
 });
 
+// Test of getName() method
 it('should be testing for for retrieval for name retrieval using getName()', () => {
     expect(manager.getName()).toBe('Mike');
 });
 
+// Test of getId() method
 it('should be testing for for retrieval for id retrieval using getId()', () => {
     expect(manager.getId()).toBe('245036');
 });
 
+// Test of getEmail() method
 it('should be testing for for retrieval for email retrieval using getEmail()', () => {
     expect(manager.getEmail()).toBe('M.Jammeson@hotmail.com');
 });
 
+// Test for getOfficeId() method
 it('should be testing for for retrieval for officeId retrieval using getOfficeId()', () => {
     expect(manager.getOfficeId()).toBe('666');
 });
 
+// Test of getRole() method
 it('should be testing for the role of the team member using getRole()', () => {
     expect(manager.getRole()).toBe('Manager');
 });
+
+// All tests must pas for Manager proile generation based on user input
